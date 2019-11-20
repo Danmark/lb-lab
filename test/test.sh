c8758c0d9053978cc265a3413ebb2a32057d33e3
Viktor Fogelberg <viktor.fogelberg@telavox.se>
2019-11-20 15:17:24 +0100

    Make buildscript fail when a test fails

--- test/test.sh
+++ test/test.sh
@@ -8,6 +8,12 @@ do
         continue
     fi
     cd "$role" || retrun
+    export test
     molecule test
+    test=$?
+    if [ "$test" -ne 0 ]
+    then
+        exit 1
+    fi
     cd "$BASE_DIR" || return
 done
